c156e45759327fa1eb8ffc81e3cc2bf9f1931a67
Leonardo Benjamin Córdova Jaramillo <lb.cordovaj@uea.edu.ec>
2026-03-15 17:23:14 -0500

    Commit 5: Archivo main.py y configuracion de .gitignore para excluir .venv e .idea

---
+++ garaje_app/main.py
@@ -0,0 +1,12 @@
+from ui.app_tkinter import AppGaraje
+
+def main():
+    """
+    Punto de entrada principal de la aplicacion 'El Hangar'.
+    Inicia el bucle principal de la interfaz grafica.
+    """
+    app = AppGaraje()
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
